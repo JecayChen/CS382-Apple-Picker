ad4afb850a6a5fdbb020492fe48ac786e6339966
JecayChen <chenjecay@gmail.com>
2024-09-20 16:50:07 -0500

    created Apple script to delete after falling

--- Apple Picker - Jecay Chen/Assets/Apple/Script/Apple.cs
+++ Apple Picker - Jecay Chen/Assets/Apple/Script/Apple.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
-{
-    // Update is called once per frame
-    void Update()
-    {
+public class Apple : MonoBehaviour{
+    // Lowest bound object Apple can
+    public static float bottomY = -20f;
+    void Update(){
+        // Apple object deletes itself after falling below lowest bound
+        if(transform.position.y < bottomY){
+            Destroy(this.gameObject);
+        }
         
     }
 }

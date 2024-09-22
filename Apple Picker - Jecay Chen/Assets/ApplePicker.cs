98c721eb01efe2fd6442d27d62b59c3a323083a6
JecayChen <chenjecay@gmail.com>
2024-09-22 08:47:27 -0500

    added score ui, basket object as lives, reset apples after losing life, and reset game after lives run out

--- Apple Picker - Jecay Chen/Assets/ApplePicker.cs
+++ Apple Picker - Jecay Chen/Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour{
     [Header("Inscribed")]
@@ -8,16 +9,38 @@ public class ApplePicker : MonoBehaviour{
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     void Start(){
+        basketList = new List<GameObject>();
+        // Idea: Summon N number of baskets on top of each other as lives at game start
         for(int i=0; i < numBaskets; i++){
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
-            pos.y = basketBottomY + (basketSpacingY * i);
-            tBasketGO.transform.position = pos;
+            pos.y = basketBottomY + (basketSpacingY * i); //Baskets with Y space apart
+            tBasketGO.transform.position = pos; 
+            basketList.Add(tBasketGO); // Tracks/Lists Basket objects
         }
     }
 
-    void Update(){
-        
+    public void AppleMissed(){
+        // Destroy all falling Apple objects
+        GameObject[] appleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach(GameObject tempGo in appleArray){
+            Destroy(tempGo);
+        }
+
+        // Destroy 1 Basket object (lives)
+        // Get index of last Basket object (top)
+        int basketIndex = basketList.Count - 1;
+        // Reference Basket object at index
+        GameObject basketGO = basketList[basketIndex];
+        // Remove Basket object from list and destroy GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        // If no Basket objects left (lives), restart game
+        if(basketList.Count == 0){
+            SceneManager.LoadScene("_Scene_0");
+        }
     }
 }
---
+++ Apple Picker - Jecay Chen/Assets/Object_Apple/Script/Apple.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour{
+    // Lowest bound object Apple can
+    public static float bottomY = -20f;
+    void Update(){
+        // Apple object deletes itself after falling below lowest bound
+        if(transform.position.y < bottomY){
+            Destroy(this.gameObject);
+
+            // Get refererence to ApplePicker component in Main Camera
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+
+            // Call public AppleMissed() of apScript
+            apScript.AppleMissed();
+        }
+        
+    }
+}
